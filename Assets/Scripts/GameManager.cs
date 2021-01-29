ebc27765d076c032ffe7a2b3a572899ac36ed6cf
CedarBishop <cedar.bishop@outlook.com>
2021-01-29 23:41:58 +1100

    Created light  that lerps position and color to day light system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,8 +9,14 @@ public class GameManager : MonoBehaviour
 
     public int score;
     public float secondsInDay;
+    public Light light;
+    public Transform lightStartLocation;
+    public Transform lightEndLocation;
+    public Color[] lightColorsOverDay;
+    public float colorLerpSpeed;
 
     private float timer;
+    private int lightColorIndex;
 
     private bool dayIsUnderway;
 
@@ -43,13 +49,17 @@ public class GameManager : MonoBehaviour
             {
                 timer -= Time.deltaTime;
             }
+            light.transform.Translate(((lightEndLocation.position - lightStartLocation.position) / secondsInDay) * Time.deltaTime);
         }
+        LerpLightColor();
     }
 
     public void StartDay ()
     {
         timer = secondsInDay;
         dayIsUnderway = true;
+        lightColorIndex = 0;
+        light.transform.position = lightStartLocation.position;
     }
 
     public void EndDay ()
@@ -61,6 +71,26 @@ public class GameManager : MonoBehaviour
     void RestartLevel ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void LerpLightColor()
+    {
+        if (lightColorsOverDay == null)
+        {
+            return;
+        }
+        
+        if (light.color == lightColorsOverDay[lightColorIndex])
+        {
+            if (lightColorIndex < lightColorsOverDay.Length - 1)
+            {
+                lightColorIndex++;
+            }
+        }
+        else
+        {
+            light.color = Color.Lerp(light.color, lightColorsOverDay[lightColorIndex], (colorLerpSpeed * lightColorsOverDay.Length / secondsInDay) * Time.deltaTime);
+        }
     }
 
 
