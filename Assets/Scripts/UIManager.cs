c2517bbaa14cbec1ef411a740630726aafc900a7
Max <madmanmax120@hotmail.com>
2021-01-30 13:02:10 +1100

    Added Basic Menu Canvases and Button Prefab

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public enum UIState {Game, Pause}
+public enum UIState {Main, Game, Pause, Options}
 
 public class UIManager : MonoBehaviour
 {
@@ -11,6 +11,12 @@ public class UIManager : MonoBehaviour
 
     public Text scoreText;
     public Image timeOfDayFillImage;
+
+    [SerializeField] private GameObject MenuMain;
+    [SerializeField] private GameObject MenuGame;
+    [SerializeField] private GameObject MenuPause;
+    [SerializeField] private GameObject MenuOptions;
+
 
     void Awake()
     {
@@ -33,6 +39,39 @@ public class UIManager : MonoBehaviour
     {
         float progress = (totalTime - timeRemaining) / totalTime;
         timeOfDayFillImage.fillAmount = progress;
+    }
+
+    public void ChangeMenu(UIState newMenu)
+    {
+        switch (newMenu)
+        {
+            case UIState.Main:
+                MenuMain.SetActive(true);
+                MenuGame.SetActive(false);
+                MenuPause.SetActive(false);
+                MenuOptions.SetActive(false);
+                break;
+            case UIState.Game:
+                MenuMain.SetActive(false);
+                MenuGame.SetActive(true);
+                MenuPause.SetActive(false);
+                MenuOptions.SetActive(false);
+                break;
+
+            case UIState.Pause:
+                MenuMain.SetActive(false);
+                MenuGame.SetActive(false);
+                MenuPause.SetActive(true);
+                MenuOptions.SetActive(false);
+
+                break;
+            case UIState.Options:
+                MenuMain.SetActive(false);
+                MenuGame.SetActive(false);
+                MenuPause.SetActive(false);
+                MenuOptions.SetActive(true);
+                break;
+        }
     }
 
 
