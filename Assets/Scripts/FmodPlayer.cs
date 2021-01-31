ac862b430ce327f7a3e752e173007f57ccbce27c
Guido Gautsch <guido.gautsch@gmail.com>
2021-01-31 12:50:06 +1100

    Added FMOD folder to project
    FMOD project and audio files are now in the Unity project. Also, there's a breath sound.

--- Assets/Scripts/FmodPlayer.cs
+++ Assets/Scripts/FmodPlayer.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class FmodPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void RunAudio(string path)
+        void RunAudio(string path)
     {
         FMODUnity.RuntimeManager.PlayOneShot(path,GetComponent<Transform>().position);
     }
+
+        void BreathAudio(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+    }
+        
 }
