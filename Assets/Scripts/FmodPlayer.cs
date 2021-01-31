93d2f941100a6a7e58c5575ef281cbafb2fccedd
Guido Gautsch <guido.gautsch@gmail.com>
2021-01-31 13:30:23 +1100

    Added jumping sound (and removed Reaper folder!)
    New jumping sounds. Also, I'd accidentally added my DAW's project folder (Reaper) to the Unity project and removed it again.

--- Assets/Scripts/FmodPlayer.cs
+++ Assets/Scripts/FmodPlayer.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class FmodPlayer : MonoBehaviour
 {
-        void RunAudio(string path)
+        void RunningSound(string path)
     {
         FMODUnity.RuntimeManager.PlayOneShot(path,GetComponent<Transform>().position);
     }
 
-        void BreathAudio(string path)
+        void BreathingSound(string path)
     {
         FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
     }
-        
+
+    void SniffingSound(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+    }
+
+    void JumpingSound(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+    }
+
+    void LandingSound(string path)
+    {
+        FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
+    }
 }
